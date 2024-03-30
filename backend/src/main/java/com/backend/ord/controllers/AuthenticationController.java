da58af87b876b99acc6f4cd01661b6cd44aaed9e
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-03-30 22:03:19 +0100

    GET auth/current-user-info endpoint added See #3

--- backend/src/main/java/com/backend/ord/controllers/AuthenticationController.java
+++ backend/src/main/java/com/backend/ord/controllers/AuthenticationController.java
@@ -2,19 +2,27 @@ package com.backend.ord.controllers;
 
 import com.backend.ord.api.requests.AuthenticationRequest;
 import com.backend.ord.api.requests.RegisterRequest;
-import com.backend.ord.api.responses.AuthenticationResponse;
+import com.backend.ord.config.security.JwtService;
+import com.backend.ord.domain.dto.UserDTO;
+import com.backend.ord.domain.entities.User;
+import com.backend.ord.domain.mappers.UserMapper;
 import com.backend.ord.exceptions.UserNotFoundException;
 import com.backend.ord.services.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
+    private final UserMapper userMapper;
+    private final JwtService jwtService;
     private final AuthenticationService authService;
 
     @PostMapping("/register")
@@ -45,5 +53,15 @@ public class AuthenticationController {
         }
     }
 
-//    @GetMapping("/current-user-info")
+    @GetMapping("/current-user-info")
+    public ResponseEntity<UserDTO> getCurrentlyLoggedInUser(
+            HttpServletRequest request
+    ) {
+        Optional<User> authenticatedUser = jwtService.getAuthenticatedUser(request);
+
+        return authenticatedUser
+                .map(user -> ResponseEntity.ok(userMapper.toDTO(user)))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.FORBIDDEN).build());
+
+    }
 }
