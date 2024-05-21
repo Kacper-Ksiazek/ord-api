0f07542ff099decaf4d72488290956da6e36ff5b
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-05-21 12:49:07 +0200

    Flyway unite practice type across `user_activities` and `practice_results` tables See #8

--- backend/src/main/resources/db/migrations/V10__create_progressions_tables.sql
+++ backend/src/main/resources/db/migrations/V10__create_progressions_tables.sql
@@ -1,8 +1,3 @@
-CALL create_enum_type(
-        'practice_type',
-        ARRAY ['CROSSWORD', 'SENTENCES_WRITING', 'WORDS_TYPING', 'GAPS']
-     );
-
 CREATE TABLE IF NOT EXISTS word_points
 (
     "word_id" UUID    NOT NULL,
@@ -13,10 +8,10 @@ CREATE TABLE IF NOT EXISTS practice_results
 (
     "id"             UUID PRIMARY KEY,
 
-    "type"           practice_type NOT NULL,
-    "score"          INTEGER       NOT NULL CHECK ( score >= 0 AND score <= 100 ),
-    "involved_words" JSONB         NOT NULL,
-    "involved_banks" JSONB         NOT NULL,
+    "type"           user_activity_type NOT NULL,
+    "score"          INTEGER            NOT NULL CHECK ( score >= 0 AND score <= 100 ),
+    "involved_words" JSONB              NOT NULL,
+    "involved_banks" JSONB              NOT NULL,
 
     "created_at"     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     "updated_at"     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
