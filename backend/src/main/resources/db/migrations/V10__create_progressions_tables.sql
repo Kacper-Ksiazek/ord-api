49c1d4c7c302a8c773ad185ca12040c8b4269aaf
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-05-28 22:26:36 +0200

    Flyway, add id, created_at, updated_at columns to the word_points table See #8

--- backend/src/main/resources/db/migrations/V10__create_progressions_tables.sql
+++ backend/src/main/resources/db/migrations/V10__create_progressions_tables.sql
@@ -1,9 +1,16 @@
+-- Points assigned to a word after a single practice session
 CREATE TABLE IF NOT EXISTS word_points
 (
-    "word_id" UUID    NOT NULL,
-    "points"  INTEGER NOT NULL CHECK ( points >= - 1 AND points <= 2)
+    "id"         UUID PRIMARY KEY,
+
+    "word_id"    UUID    NOT NULL,
+    "points"     INTEGER NOT NULL CHECK ( points >= - 1 AND points <= 2),
+
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+-- The summary result of one practice session
 CREATE TABLE IF NOT EXISTS practice_results
 (
     "id"             UUID PRIMARY KEY,
