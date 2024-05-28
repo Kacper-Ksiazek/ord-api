e7dd8949f849a13151f11c0ad4f50ccabf03337c
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-05-28 22:27:24 +0200

    Flyway / change check clause for `used_gpt_tokens` in the user_activites table See #8

--- backend/src/main/resources/db/migrations/V9__create_user_activities_table.sql
+++ backend/src/main/resources/db/migrations/V9__create_user_activities_table.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS user_activities
     "status"          user_activity_status NOT NULL DEFAULT 'IN_PROGRESS',
     "acquired_points" INTEGER              NOT NULL CHECK ( acquired_points >= 0 AND acquired_points <= 1000),
 
-    "used_gpt_tokens" INTEGER              NOT NULL DEFAULT 0 CHECK ( used_gpt_tokens >= 0 AND used_gpt_tokens <= 1000),
+    "used_gpt_tokens" INTEGER              NOT NULL DEFAULT 0 CHECK ( used_gpt_tokens >= 0),
 
     "created_at"      TIMESTAMP WITH TIME ZONE      DEFAULT CURRENT_TIMESTAMP,
     "updated_at"      TIMESTAMP WITH TIME ZONE      DEFAULT CURRENT_TIMESTAMP
