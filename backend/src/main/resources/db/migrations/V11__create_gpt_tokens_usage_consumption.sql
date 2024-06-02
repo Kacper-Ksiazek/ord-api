6f5fc7370ebfe7463c7c180d6bc5f6227c4eadef
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-06-02 19:24:12 +0200

    Flyway / Update id's column types to be UUID See #8

--- backend/src/main/resources/db/migrations/V11__create_gpt_tokens_usage_consumption.sql
+++ backend/src/main/resources/db/migrations/V11__create_gpt_tokens_usage_consumption.sql
@@ -11,14 +11,14 @@ CALL create_enum_type(
 
 CREATE TABLE IF NOT EXISTS gpt_tokens_consumption
 (
-    id               SERIAL PRIMARY KEY,
+    id               UUID PRIMARY KEY,
 
-    user_id          INTEGER              NOT NULL,
+    user_id          UUID                 REFERENCES users (id) ON DELETE SET NULL,
     consumption_type gpt_consumption_type NOT NULL,
 
     -- Number of Open AI tokens consumed
     number_of_tokens INTEGER              NOT NULL CHECK (number_of_tokens >= 0),
 
-    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    "created_at"     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updated_at"     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
