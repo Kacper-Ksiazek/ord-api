a68f5eb4f3c9e6a4bb4606bdd800a3ae3946d5d6
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-07-08 22:18:12 +0200

    flyway / update flyway migrations See #12

--- backend/src/main/resources/db/migrations/V9__create_stories_table.sql
+++ backend/src/main/resources/db/migrations/V9__create_stories_table.sql
@@ -1,18 +1,15 @@
 CREATE TABLE IF NOT EXISTS "stories"
 (
-    id               UUID PRIMARY KEY,
+    id           UUID PRIMARY KEY,
 
-    user_id          UUID        REFERENCES users (id) ON DELETE SET NULL,
+    user_id      UUID        REFERENCES users (id) ON DELETE SET NULL,
 
-    title            VARCHAR(64) NOT NULL,
-    content          TEXT        NOT NULL,
+    title        VARCHAR(64) NOT NULL,
+    content      TEXT        NOT NULL,
 
     -- Map<String, String> of explanations
-    explanations     JSONB       NOT NULL,
+    explanations JSONB       NOT NULL,
 
-    -- Number of Open AI tokens consumed
-    number_of_tokens INTEGER     NOT NULL CHECK (number_of_tokens >= 0),
-
-    "created_at"     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    "updated_at"     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
