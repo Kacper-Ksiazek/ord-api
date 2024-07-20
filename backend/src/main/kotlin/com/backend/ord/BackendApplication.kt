a43cc21b99de40f67ba4f1092ea8935f5f292a43
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-07-20 23:24:53 +0200

    Add db connection verification to the application run method See #12

--- backend/src/main/kotlin/com/backend/ord/BackendApplication.kt
+++ backend/src/main/kotlin/com/backend/ord/BackendApplication.kt
@@ -5,27 +5,40 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.core.env.Environment
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
 @EnableTransactionManagement
 class BackendApplication(
-    private val environment: Environment
+    private val environment: Environment,
+    private val jdbcTemplate: JdbcTemplate
 ) : CommandLineRunner {
 
     override fun run(vararg args: String) {
-        Console.printCyan("The main application has started")
+        Console.printCyan("\nThe main application is booting up...")
         Console.addBreakLine(1)
 
         verifyEnvironment()
+        verifyDatabaseConnection()
+
+        Console.addBreakLine(1)
+        Console.printGreen("The main application has started successfully!")
+        Console.addBreakLine(3)
     }
 
     private fun verifyEnvironment() {
-        Console.ensureFunctionSuccess("Verify environment") {
+        Console.ensureFunctionSuccess("1. Verify environment") {
             val testingProperty = environment.getProperty("ENV_TEST_PROPERTY")
             if (testingProperty != "1test1") {
                 throw RuntimeException("Environment property verification failed")
             }
+        }
+    }
+
+    private fun verifyDatabaseConnection() {
+        Console.ensureFunctionSuccess("2. Verify database connection") {
+            jdbcTemplate.execute("SELECT 1")
         }
     }
 }
