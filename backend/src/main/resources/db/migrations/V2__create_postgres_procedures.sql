d4e9ec04141a1c75fc5bd00a2b792efcd110a8bd
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-07-20 15:00:01 +0200

    Organize docker compose & application properties See #12

--- backend/src/main/resources/db/migrations/V2__create_postgres_procedures.sql
+++ backend/src/main/resources/db/migrations/V2__create_postgres_procedures.sql
@@ -6,18 +6,33 @@ CREATE OR REPLACE PROCEDURE create_enum_type(
 AS
 $$
 BEGIN
-    EXECUTE format(
-            'CREATE TYPE %I AS ENUM (%s)',
-            type_name,
-            array_to_string(
-                    array(
-                            SELECT quote_literal(value)
-                            FROM unnest(enum_values) AS value
-                    ),
-                    ', '
-            )
-            );
-EXCEPTION
-    WHEN duplicate_object THEN NULL;
+    BEGIN
+        EXECUTE format(
+                'CREATE TYPE %I AS ENUM (%s)',
+                type_name,
+                array_to_string(
+                        array(
+                                SELECT quote_literal(value)
+                                FROM unnest(enum_values) AS value
+                        ),
+                        ', '
+                )
+                );
+    EXCEPTION
+        WHEN duplicate_object THEN
+            -- Ignore the exception if the type already exists
+            NULL;
+    END;
+
+    BEGIN
+        EXECUTE format(
+                'CREATE CAST (VARCHAR AS %I) WITH INOUT AS IMPLICIT',
+                type_name
+                );
+    EXCEPTION
+        WHEN duplicate_object THEN
+            -- Ignore the exception if the cast already exists
+            NULL;
+    END;
 END;
 $$;
