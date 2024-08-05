63c214674e53f6867d8352bb1cefbc11145e736e
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-08-05 23:08:42 +0200

    Introduce WordRequestFactory utility class See #20

---
+++ backend/src/test/kotlin/com/backend/ord/controllers/request_factories/WordRequestFactory.kt
@@ -0,0 +1,74 @@
+package com.backend.ord.controllers.request_factories
+
+import com.backend.ord.api.requests.bank.CreateBankRequest
+import com.backend.ord.api.requests.word.CreateWordRequest
+import com.backend.ord.controllers.utils_for_testing.MockedAuthenticatedUser
+import com.backend.ord.domain.embedded.ExampleSentence
+import com.backend.ord.enums.Language.LanguageName
+import com.backend.ord.enums.Word.WordExtraMark
+import com.backend.ord.enums.Word.WordType
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import java.util.UUID
+
+class WordRequestFactory(
+    private val BASE_URL: String,
+    private val objectMapper: ObjectMapper
+) {
+    fun getSingleWordRequest() {
+        // TODO
+    }
+
+    fun getAllWordsRequest() {
+        // TODO
+    }
+
+    /**
+     * POST /words/
+     */
+    fun createWordRequest(
+        origin: String = "word in english",
+        translation: String = "slowo po polsku",
+        translatedFrom: LanguageName = LanguageName.ENGLISH,
+        translatedTo: LanguageName = LanguageName.POLISH,
+        type: WordType = WordType.NOUN,
+        exampleSentences: Set<ExampleSentence> = setOf(
+            ExampleSentence(
+                sentence = "example sentence",
+                translation = "przykladowe zdanie"
+            ),
+            ExampleSentence(
+                sentence = "another example sentence",
+                translation = "kolejne przykladowe zdanie"
+            )
+        ),
+        authenticatedUser: MockedAuthenticatedUser? = null,
+        extraMark: WordExtraMark? = WordExtraMark.SLANG,
+        bankId: UUID? = null,
+        bankToCreate: CreateBankRequest? = null
+
+    ): MockHttpServletRequestBuilder {
+        return MockMvcRequestBuilders.post(BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(
+                objectMapper.writeValueAsString(
+                    CreateWordRequest(
+                        origin = origin,
+                        translation = translation,
+                        translatedFrom = translatedFrom,
+                        translatedTo = translatedTo,
+                        type = type,
+                        exampleSentences = exampleSentences,
+                        extraMark = extraMark,
+                        bankId = bankId,
+                        bankToCreate = bankToCreate
+                    )
+                )
+            ).apply {
+                if (authenticatedUser != null) this.cookie(authenticatedUser.authCookie)
+            }
+    }
+}
