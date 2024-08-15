93f87e6328f016e02b9c4c84f550c87fc75b9805
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-08-15 22:55:11 +0200

    Update RESTExceptionHandler to catch every exception and subsequently return http 500 response See #20

--- backend/src/main/kotlin/com/backend/ord/exceptions/handlers/RESTExceptionHandler.kt
+++ backend/src/main/kotlin/com/backend/ord/exceptions/handlers/RESTExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.backend.ord.exceptions.handlers
 
 import com.backend.ord.api.responses.HTTPErrorResponse
 import com.backend.ord.exceptions.REST.*
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -21,16 +22,38 @@ class RESTExceptionHandler {
     fun handleException(e: Exception): ResponseEntity<HTTPErrorResponse> {
         val status = getStatusForException(e)
         val errorResponse = HTTPErrorResponse(message = e.message, status = status)
+
         return ResponseEntity.status(status).body(errorResponse)
     }
 
+    // Catch-all exception handler
+    @ExceptionHandler(Exception::class)
+    fun handleUncaughtException(e: Exception): ResponseEntity<HTTPErrorResponse> {
+        val errorResponse = HTTPErrorResponse(
+            message = """
+                Unexpected error occurred
+                - Exception: $e
+                - Message: ${e.message}
+                - Cause: ${e.cause}
+                - Stack trace: ${e.stackTrace}
+                - Class: ${e.javaClass}
+                - Localized message: ${e.localizedMessage}
+                - Suppressed: ${e.suppressed}
+            """,
+
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value()
+        )
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse)
+    }
+
     private fun getStatusForException(e: Exception): Int = when (e) {
-        is BadRequestException -> 400
-        is UnauthorizedException -> 401
-        is ForbiddenException -> 403
-        is NotFoundException -> 404
-        is PayloadTooLargeException -> 413
-        is InternalServerError -> 500
-        else -> 500
+        is BadRequestException -> HttpStatus.BAD_REQUEST.value() // 400
+        is UnauthorizedException -> HttpStatus.UNAUTHORIZED.value() // 401
+        is ForbiddenException -> HttpStatus.FORBIDDEN.value() // 403
+        is NotFoundException -> HttpStatus.NOT_FOUND.value() // 404
+        is PayloadTooLargeException -> HttpStatus.PAYLOAD_TOO_LARGE.value() // 413
+        is InternalServerError -> HttpStatus.INTERNAL_SERVER_ERROR.value() // 500
+        else -> HttpStatus.INTERNAL_SERVER_ERROR.value() // 500
     }
 }
