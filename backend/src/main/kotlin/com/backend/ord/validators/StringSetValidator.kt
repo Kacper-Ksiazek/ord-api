e455ace05d57fc634324ee49ebd33ab45787d5b4
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-08-17 13:55:22 +0200

    Create a Set<String> custom validator See #20

---
+++ backend/src/main/kotlin/com/backend/ord/validators/StringSetValidator.kt
@@ -0,0 +1,34 @@
+package com.backend.ord.validators
+
+import com.backend.ord.validators.annotations.ValidStringSet
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+
+class StringSetValidator : ConstraintValidator<ValidStringSet, Set<String>> {
+    private var minSetSize: Int = 1;
+    private var maxSetSize: Int = 5;
+
+    private var minElementSize: Int = 1;
+    private var maxElementSize: Int = 255;
+    override fun initialize(constraintAnnotation: ValidStringSet) {
+        super.initialize(constraintAnnotation)
+
+        minSetSize = constraintAnnotation.minSetSize
+        maxSetSize = constraintAnnotation.maxSetSize
+
+        minElementSize = constraintAnnotation.minElementSize
+        maxElementSize = constraintAnnotation.maxElementSize
+    }
+
+    override fun isValid(value: Set<String>?, context: ConstraintValidatorContext): Boolean {
+        if (value == null) return true // or false if null sets are invalid
+
+        // Validate set size
+        if (value.size in minSetSize..maxSetSize) {
+            // Validate element size
+            return value.all { it.length in minElementSize..maxElementSize }
+        }
+
+        return false
+    }
+}
