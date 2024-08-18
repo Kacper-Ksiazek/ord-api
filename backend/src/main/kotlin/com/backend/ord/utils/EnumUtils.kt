eddf78a4f3c6b5d830cdf7dde9b5d1a5a880a016
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-08-18 22:37:32 +0200

    Create a new util in EnumUtils - getRandomValueOrNull See #20

--- backend/src/main/kotlin/com/backend/ord/utils/EnumUtils.kt
+++ backend/src/main/kotlin/com/backend/ord/utils/EnumUtils.kt
@@ -1,5 +1,7 @@
 package com.backend.ord.utils
 
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import kotlin.reflect.KClass
 
 object EnumUtils {
@@ -43,5 +45,39 @@ object EnumUtils {
      */
     fun <T : Enum<T>> KClass<T>.getRandomValue(): T {
         return this.java.enumConstants.random()
+    }
+
+
+    /**
+     * Returns a random enum constant from the specified enum class, or `null` with the specified probability.
+     * The probability of returning `null` is determined by the `changesForNull` parameter.
+     * If the randomly generated integer is greater than `changesForNull`, a random enum constant is returned.
+     * Otherwise, `null` is returned.
+     *
+     * This extension function is applied to a `KClass` representing an enum type.
+     *
+     * @return A random enum constant from the enum class, or `null` with the specified probability.
+     *
+     * @param changesForNull The probability of returning `null`. It must be an integer between 1 and 99.
+     *                      The default value is 33, which means there is a 33% chance of returning `null`.
+     *
+     * @param T The type of the enum class.
+     *
+     * @sample
+     * enum class Color { RED, GREEN, BLUE }
+     *
+     * val randomColor = Color::class.getRandomValueOrNull()
+     *
+     * // Example output:
+     * // randomColor: RED | GREEN | BLUE | null
+     */
+    fun <T : Enum<T>> KClass<T>.getRandomValueOrNull(
+        @Min(1) @Max(99) changesForNull: Int = 33
+    ): T? {
+        val randomInt = (1..100).random()
+
+        return randomInt.takeIf { it > changesForNull }?.let {
+            this.java.enumConstants.random()
+        }
     }
 }
