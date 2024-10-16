f2e28ae44e4e5f9133146cf8c9dc9b856f7d9ae8
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2024-10-17 01:12:29 +0200

    Create GPTTokensConsumptionController blueprint

---
+++ backend/src/main/kotlin/com/backend/ord/controllers/GPTTokensConsumptionController.kt
@@ -0,0 +1,18 @@
+package com.backend.ord.controllers
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/gpt-tokens-consumption")
+class GPTTokensConsumptionController {
+    // TODO: Implement following endpoints:
+    // 1. GET / - Get all GPT tokens consumption ( words, stories, games ) for the current user
+    // 2. GET /words - Get all GPT tokens consumption for words for the current user
+    // 3. GET /stories - Get all GPT tokens consumption for stories for the current user
+    // 4. GET /games - Get all GPT tokens consumption for games for the current user
+    // 5. GET /summary - Get summary of GPT tokens consumption for the current user
+
+
+
+}
