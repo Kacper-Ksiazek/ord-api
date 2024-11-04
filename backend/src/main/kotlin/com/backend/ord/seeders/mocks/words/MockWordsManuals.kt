82c693525fb37277d988560b42c5a42ce416e33d
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2024-11-04 23:07:12 +0100

    Add random generation of bookmark flag to mocked from JSON file words

--- backend/src/main/kotlin/com/backend/ord/seeders/mocks/words/MockWordsManuals.kt
+++ backend/src/main/kotlin/com/backend/ord/seeders/mocks/words/MockWordsManuals.kt
@@ -3,12 +3,12 @@ package com.backend.ord.seeders.mocks.words
 import com.backend.ord.domain.entities.User
 import com.backend.ord.domain.entities.Word
 import com.backend.ord.enums.Language.LanguageName
-import com.backend.ord.enums.Word.WordType
 import com.backend.ord.repositories.WordRepository
 import com.backend.ord.seeders.mocks.words.bases.MocksFromJsonFileHandler
 import com.backend.ord.seeders.mocks.words.json_data_models.AIGeneratedWordManual
 import com.fasterxml.jackson.core.type.TypeReference
 import org.springframework.stereotype.Component
+import kotlin.random.Random
 
 @Component
 class MockWordsManuals(
@@ -45,6 +45,8 @@ class MockWordsManuals(
 
             useCases = jsonData.useCases.toSet(),
             exampleSentences = jsonData.exampleSentences.toSet(),
+
+            isBookmarked = Random.nextBoolean(),
 
             user = user,
             userId = user.id
