f0da9f21d12d71c3b979873f6be5db5fd0e498bf
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2024-11-28 18:58:15 +0100

    feat: Prepare GetManyWordsRequest interface

---
+++ backend/src/main/kotlin/com/backend/ord/api/requests/word/GetManyWordsRequest.kt
@@ -0,0 +1,28 @@
+package com.backend.ord.api.requests.word
+
+import com.backend.ord.enums.Language.LanguageName
+import com.backend.ord.enums.Word.WordExtraMark
+import com.backend.ord.enums.Word.WordType
+import com.backend.ord.api.requests.enums.SortDirection
+import com.backend.ord.api.requests.word.enums.GetAllWordsSortOptions
+
+import java.util.UUID
+
+
+interface GetManyWordsRequest {
+    val language: LanguageName
+
+    val page: Int?
+    val perPage: Int?
+
+    val wordType: WordType?
+    val searchingPhrase: String?
+    val wordExtraMark: WordExtraMark?
+    val bookmarkedOnly: Boolean?
+
+    val banksIds: List<UUID>?
+    val bankGroupsIds: List<UUID>?
+
+    val sortDirection: SortDirection?
+    val sortBy: GetAllWordsSortOptions?
+}
