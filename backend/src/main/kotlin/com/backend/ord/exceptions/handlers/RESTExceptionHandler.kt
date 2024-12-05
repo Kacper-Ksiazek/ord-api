88827c1b58166b20d472305221ac0a9d59f1646d
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2024-12-05 18:06:08 +0100

    fix: Remove no longer needed TODO comments

--- backend/src/main/kotlin/com/backend/ord/exceptions/handlers/RESTExceptionHandler.kt
+++ backend/src/main/kotlin/com/backend/ord/exceptions/handlers/RESTExceptionHandler.kt
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 
-/**
- * TODO: Add docs
- */
 @ControllerAdvice
 class RESTExceptionHandler {
     @ExceptionHandler(
