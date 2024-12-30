7fa09c632db8b88b1d894cff94a6e8f726b6118a
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2024-12-30 01:22:00 +0100

    fix: Assign game's grade in finishGame

--- backend/src/main/kotlin/com/backend/ord/services/impl/GameServiceImpl.kt
+++ backend/src/main/kotlin/com/backend/ord/services/impl/GameServiceImpl.kt
@@ -1,9 +1,11 @@
 package com.backend.ord.services.impl
 
 import com.backend.ord.domain.persistence.entities.Game
+import com.backend.ord.enums.persistence.game.GameGrade
 import com.backend.ord.enums.persistence.game.GameStatus
 import com.backend.ord.repositories.bases.UserResourceRepository
 import com.backend.ord.services.GameService
+import com.backend.ord.utils.data_classes.Percentage
 import org.springframework.stereotype.Service
 
 @Service
@@ -17,7 +19,9 @@ class GameServiceImpl(
     ): Game {
         game.finalScore = finalScore
         game.duration = duration
+
         game.status = GameStatus.COMPLETED
+        game.grade = GameGrade.fromPercentage(Percentage(finalScore))
 
         return repository.save(game)
     }
