bdd57a0b6aa9f3b3c2156d4dacca90c0afbef9ed
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-01-28 15:30:53 +0100

    feat: Prepare GameRequestFactory.kt

---
+++ backend/src/test/kotlin/com/backend/ord/controllers/request_factories/GameRequestFactory.kt
@@ -0,0 +1,44 @@
+package com.backend.ord.controllers.request_factories
+
+import com.backend.ord.api.requests.games.data.StartGameRequestData
+import com.backend.ord.controllers.utils_for_testing.MockedAuthenticatedUser
+import com.backend.ord.enums.persistence.game.GameDifficulty
+import com.backend.ord.enums.persistence.game.GameType
+import com.backend.ord.enums.persistence.language.LanguageName
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+
+private fun GameType.getStartGameAPIUrl(): String {
+    return "/api/v1/games/${this.name.lowercase()}/start"
+}
+
+class GameRequestFactory(
+    private val objectMapper: ObjectMapper,
+) {
+
+    fun startGameRequest(
+        gameType: GameType,
+        difficulty: GameDifficulty = GameDifficulty.HARD,
+        language: LanguageName = LanguageName.ENGLISH,
+        authenticatedUser: MockedAuthenticatedUser? = null,
+    ): MockHttpServletRequestBuilder {
+        return MockMvcRequestBuilders
+            .post(gameType.getStartGameAPIUrl())
+            .apply {
+                if (authenticatedUser != null) this.cookie(authenticatedUser.authCookie)
+            }
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(
+                objectMapper.writeValueAsString(
+                    StartGameRequestData(
+                        difficulty = difficulty,
+                        language = language
+                    )
+                )
+            )
+    }
+
+}
