7fce5f1082d60ee6d16f18c941c6862218b1bd44
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-02-23 19:31:26 +0100

    feat: Prepare GameSeeder

--- backend/src/main/kotlin/com/backend/ord/seeders/entities/SeederInterface.kt
+++ backend/src/main/kotlin/com/backend/ord/seeders/entities/SeederInterface.kt
@@ -4,7 +4,9 @@ interface SeederInterface<T> {
     /**
      * Insert a singular row into the database
      */
-    fun seedOneEntity(data: T? = null): T
+    fun seedOneEntity(data: T? = null): T {
+        throw RuntimeException("Method not allowed")
+    }
 
     /**
      * Delete all rows from the database
