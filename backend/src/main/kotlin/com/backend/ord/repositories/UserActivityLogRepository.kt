af3e67922ab8dd90d32908e623ce6da1635abaa2
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-03-04 23:59:06 +0100

    feat: Prepare a repository mapping for the newly redefined entity - UserActivityLog

---
+++ backend/src/main/kotlin/com/backend/ord/repositories/UserActivityLogRepository.kt
@@ -0,0 +1,8 @@
+package com.backend.ord.repositories
+
+import com.backend.ord.domain.persistence.entities.UserActivityLog
+import com.backend.ord.repositories.bases.UserResourceRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserActivityLogRepository : UserResourceRepository<UserActivityLog>
