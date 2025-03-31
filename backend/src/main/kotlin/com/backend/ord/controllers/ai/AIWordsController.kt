5bc0d0fbe7faa5aa671cc432a1dca6b9f77d6e84
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-03-31 15:14:56 +0200

    refactor: Replace OpenAIAccessController with the AIWordsController

--- backend/src/main/kotlin/com/backend/ord/controllers/ai/AIWordsController.kt
+++ backend/src/main/kotlin/com/backend/ord/controllers/ai/AIWordsController.kt
@@ -1,10 +1,9 @@
-package com.backend.ord.controllers
+package com.backend.ord.controllers.ai
 
 import com.backend.ord.api.requests.openai.OpenAIRequestFactory
 import com.backend.ord.api.responses.GenerateWordManualAIResponse
 import com.backend.ord.config.RestClientConfig
 import com.backend.ord.config.security.JwtService
-import com.backend.ord.domain.persistence.embedded.ExampleSentence
 import com.backend.ord.domain.persistence.entities.LanguageProficiency
 import com.backend.ord.domain.persistence.entities.User
 import com.backend.ord.enums.persistence.language.LanguageName
@@ -24,9 +23,10 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
-@RequestMapping("/api/v1/openai")
-class OpenAIAccessController(
+@RequestMapping("/api/v1/ai/words")
+class AIWordsController(
     private val jwtService: JwtService,
     private val restClientConfig: RestClientConfig,
     private val openAIRequestFactory: OpenAIRequestFactory,
@@ -35,43 +35,7 @@ class OpenAIAccessController(
 ) {
     private val jsonObjectMapper: ObjectMapper = jacksonObjectMapper()
 
-    // TODO: Remove this since it was just a POC
-    @Deprecated("Use generateWordManual instead")
-    @GetMapping("/examples-of-usage")
-    fun generateExamplesOfUsage(
-        request: HttpServletRequest,
-        @RequestParam level: LanguageProficiencyLevel,
-        @RequestParam language: LanguageName,
-        @RequestParam word: String,
-        @RequestParam(required = false) translateExamplesTo: LanguageName?,
-        @RequestParam(defaultValue = "3") examplesCount: Int
-    ): ResponseEntity<*> {
-        val user: User = jwtService.getAuthenticatedUserOrThrowForbidden(request)
-
-        // Create the request
-        val openAIRequest = openAIRequestFactory.createRequest(
-            prompt = String.format(
-                "Generate %d example sentences in %s language with %s level of proficiency for the word \"%s\". Moreover, for each example generate also its translation to %s language.",
-                examplesCount,
-                language.name,
-                level.name,
-                word,
-                translateExamplesTo?.name ?: user.nativeLanguage
-            ),
-            context = "Generate response in JSON format matching following typescript interface: type response = { sentence: string, translation: string }[]. I want my answer to be suitable for any JSON parser such as Jackson or JSON.parse from js. Do not add any markdown formatting around the examples, just raw JSON. Additionally, wrap the word in the examples with asterisks to highlight it as well as the part of the translation that corresponds to the word.",
-        )
-
-        // Send the request to OpenAI
-        val response = restClientConfig.makeOpenAIPostRequest(openAIRequest)
-
-        // Parse request into List<String>
-        val examples: Set<ExampleSentence> = jsonObjectMapper.readValue(response.data)
-
-        // Return 200 ok code
-        return ResponseEntity.ok().body(examples)
-    }
-
-    @GetMapping("/generate-word-manual")
+    @GetMapping("/generate-manual")
     fun generateWordManual(
         request: HttpServletRequest,
         @RequestParam word: String,
