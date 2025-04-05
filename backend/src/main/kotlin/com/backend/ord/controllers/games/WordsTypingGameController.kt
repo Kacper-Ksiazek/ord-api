d1e90628f1814093b9e518ccdbf6a4ce6e8e3b39
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-04-06 00:47:42 +0200

    feat: WordsTypingGameController - implement start crossword endpoint

--- backend/src/main/kotlin/com/backend/ord/controllers/games/WordsTypingGameController.kt
+++ backend/src/main/kotlin/com/backend/ord/controllers/games/WordsTypingGameController.kt
@@ -1,10 +1,17 @@
 package com.backend.ord.controllers.games
 
 import com.backend.ord.api.requests.games.data.StartGameRequestData
+import com.backend.ord.api.responses.games.bases.StartedWordsTypingGameResponse
 import com.backend.ord.controllers.games.bases.GameControllerBase
+import com.backend.ord.domain.application.games.words_typing.WordsTypingInstruction
+import com.backend.ord.domain.application.games.words_typing.WordsTypingQuestion
+import com.backend.ord.domain.persistence.entities.OngoingGame
 import com.backend.ord.domain.persistence.entities.User
+import com.backend.ord.enums.persistence.game.GameType
+import com.backend.ord.utils.hideLetters
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,16 +25,40 @@ class WordsTypingGameController : GameControllerBase() {
     fun startCrosswordGame(
         request: HttpServletRequest,
         @Valid @RequestBody body: StartGameRequestData
-//    ): ResponseEntity<StartedCrosswordGameResponse> {
-    ) {
-        // 1. Assert the user is authenticated
+    ): ResponseEntity<StartedWordsTypingGameResponse> {
         val user: User = jwtService.getAuthenticatedUserOrThrowForbidden(request)
 
-        // 2. Generate crossword game using AI
         val (aiResponse, properAnswers) = aiGameService.generateWordsTypingGame(
             user = user,
             language = body.language,
             difficulty = body.difficulty
         )
+
+        val instruction: WordsTypingInstruction = aiResponse.entries.map {
+            WordsTypingQuestion(
+                word = it.key.hideLetters(),
+                clue = it.value
+            )
+        }
+
+        val savedGame: OngoingGame = ongoingGameService.save(
+            OngoingGame(
+                user = user,
+                difficulty = body.difficulty,
+                type = GameType.WORDS_TYPING,
+                language = body.language,
+                properAnswers = jsonObjectMapper.writeValueAsString(properAnswers)
+            )
+        )
+
+        return ResponseEntity.ok(
+            StartedWordsTypingGameResponse(
+                gameId = savedGame.id,
+                instruction = instruction,
+                properAnswers = properAnswers
+            )
+        )
     }
+
+
 }
