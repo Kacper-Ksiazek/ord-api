7a63a60b6bce54bd034ae219f12224a9e68961d7
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-07-15 22:29:41 +0200

    refactor(SentencesWritingGameController): Use @AuthenticatedUser annotation

--- src/main/kotlin/com/ord/features/game/api/controllers/SentencesWritingGameController.kt
+++ src/main/kotlin/com/ord/features/game/api/controllers/SentencesWritingGameController.kt
@@ -1,14 +1,10 @@
 package com.ord.features.game.api.controllers
 
+import com.ord.core.auth.security.AuthenticatedUser
 import com.ord.core.user.model.UserEntity
-import com.ord.features.game.model.ongoing_game.OngoingGameEntity
-import com.ord.features.game.model.ongoing_game.enums.GameType
-import com.ord.features.game.variants.crossword.dto.api_requests.FinishWordsTypingGameRequest
-import com.ord.features.game.variants.shared.api.GameControllerBase
+import com.ord.features.game.variants.sentences_writing.api.SentencesWritingGameFacade
+import com.ord.features.game.variants.sentences_writing.dto.api_responses.StartedSentencesWritingGameResponse
 import com.ord.features.game.variants.shared.dto.api_requests.StartGameRequest
-import com.ord.features.game.variants.words_typing.dto.api_responses.FinishedWordsTypingGameResponse
-import com.ord.features.game.variants.words_typing.dto.api_responses.StartedWordsTypingGameResponse
-import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,43 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/games/sentences-writting")
-class SentencesWritingGameController : GameControllerBase() {
+@RequestMapping("/api/v1/games/sentences-writing")
+class SentencesWritingGameController(
+    private val sentencesWritingGameFacade: SentencesWritingGameFacade
+) {
 
     @PostMapping("/start")
     fun startWordsTypingGame(
-        request: HttpServletRequest,
+        @AuthenticatedUser user: UserEntity,
         @Valid @RequestBody body: StartGameRequest
-    ): ResponseEntity<StartedWordsTypingGameResponse> {
-        val user: UserEntity = jwtService.getAuthenticatedUserOrThrowForbidden(request)
-
-        val instruction = aiGameService.generateSentencesWritingGame(
-            user = user,
-            language = body.language,
-            difficulty = body.difficulty
-        )
-
-        val savedGame: OngoingGameEntity = ongoingGameService.save(
-            OngoingGameEntity(
-                user = user,
-                type = GameType.WORDS_TYPING,
-
-                language = body.language,
-                difficulty = body.difficulty,
-                properAnswers = jsonObjectMapper.writeValueAsString(null)
-            )
-        )
-
-        return ResponseEntity.ok(
-            TODO()
-        )
-    }
+    ): ResponseEntity<StartedSentencesWritingGameResponse> = sentencesWritingGameFacade.startGame(user, body)
 
     @PostMapping("/finish")
     fun finishWordsTypingGame(
-        request: HttpServletRequest,
-        @Valid @RequestBody body: FinishWordsTypingGameRequest
-    ): ResponseEntity<FinishedWordsTypingGameResponse> {
-        TODO()
-    }
+        @AuthenticatedUser user: UserEntity,
+        @Valid @RequestBody body: Int
+    ): ResponseEntity<Int> = sentencesWritingGameFacade.finishGame(user, body)
 }
