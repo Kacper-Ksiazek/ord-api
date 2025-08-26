75becfe8c4902a75ce6c86ffef4fa3701f20d828
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-08-26 20:03:21 +0200

    feat: refactor AuthAPIClient to extend APITestClient and implement authentication methods

--- src/test/kotlin/com/ord/testing_utils/api/APITestClient.kt
+++ src/test/kotlin/com/ord/testing_utils/api/APITestClient.kt
@@ -1,32 +1,101 @@
 package com.ord.testing_utils.api
 
 import com.ord.testing_utils.dto.MockedAuthenticatedUser
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 
-interface APITestClient {
+abstract class APITestClient(
+    val webClient: WebTestClient
+) {
     fun get(
         url: String,
         user: MockedAuthenticatedUser?,
         queryParams: Map<String, String>? = null,
-    ): WebTestClient.ResponseSpec
+    ): WebTestClient.ResponseSpec {
+        val request = webClient
+            .get()
+            .uri { uriBuilder ->
+                var builder = uriBuilder.path(url)
+                queryParams?.forEach { (key, value) ->
+                    builder = builder.queryParam(key, value)
+                }
+                builder.build()
+            }
+            .apply {
+                withAuth(user)
+
+                if (user != null) {
+                    this.cookie(user.authCookie.name, user.authCookie.value)
+                }
+            }
+
+        return request
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+    }
 
 
     fun <TBody> post(
         url: String,
-        body: TBody? = null,
-        user: MockedAuthenticatedUser?,
-    ): WebTestClient.ResponseSpec
+        body: TBody?,
+        user: MockedAuthenticatedUser?
+    ): WebTestClient.ResponseSpec {
+        return webClient
+            .post()
+            .uri(url)
+            .contentType(MediaType.APPLICATION_JSON)
+            .apply {
+                withAuth(user)
+
+                if (body != null) {
+                    this.bodyValue(body)
+                }
+            }
+            .exchange()
+    }
 
 
     fun <TBody> put(
         url: String,
-        body: TBody? = null,
-        user: MockedAuthenticatedUser?,
-    ): WebTestClient.ResponseSpec
+        body: TBody?,
+        user: MockedAuthenticatedUser?
+    ): WebTestClient.ResponseSpec {
+        return webClient
+            .put()
+            .uri(url)
+            .contentType(MediaType.APPLICATION_JSON)
+            .apply {
+                withAuth(user)
+
+                if (body != null) {
+                    this.bodyValue(body)
+                }
+            }
+            .exchange()
+    }
 
 
     fun delete(
         url: String,
+        user: MockedAuthenticatedUser?
+    ): WebTestClient.ResponseSpec {
+        return webClient
+            .delete()
+            .uri(url)
+            .apply {
+                withAuth(user)
+            }
+            .exchange()
+    }
+
+
+    private fun WebTestClient.RequestHeadersSpec<*>.withAuth(
         user: MockedAuthenticatedUser?,
-    ): WebTestClient.ResponseSpec
+    ): WebTestClient.RequestHeadersSpec<*> {
+        return if (user != null) {
+            this.cookie(user.authCookie.name, user.authCookie.value)
+        } else {
+            this
+        }
+    }
 }
--- src/test/kotlin/com/ord/testing_utils/api/clients/AuthAPIClient.kt
+++ src/test/kotlin/com/ord/testing_utils/api/clients/AuthAPIClient.kt
@@ -2,28 +2,41 @@ package com.ord.testing_utils.api.clients
 
 import com.ord.core.auth.api.requests.dto.LoginRequest
 import com.ord.core.auth.api.requests.dto.RegisterRequest
+import com.ord.testing_utils.api.APITestClient
 import com.ord.testing_utils.dto.MockedAuthenticatedUser
 import org.springframework.test.web.reactive.server.WebTestClient
 
-interface AuthAPIClient {
+class AuthAPIClient(
+    webClient: WebTestClient
+) : APITestClient(webClient) {
+    val baseUrl = "/api/v1/auth"
+
     fun register(
         body: RegisterRequest,
-        user: MockedAuthenticatedUser? = null,
-    ): WebTestClient.ResponseSpec
+        user: MockedAuthenticatedUser? = null
+    ): WebTestClient.ResponseSpec {
+        return post("$baseUrl/register", body, user)
+    }
 
 
     fun login(
         body: LoginRequest,
-        user: MockedAuthenticatedUser? = null,
-    ): WebTestClient.ResponseSpec
+        user: MockedAuthenticatedUser? = null
+    ): WebTestClient.ResponseSpec {
+        return post("$baseUrl/login", body, user)
+    }
 
 
     fun me(
-        user: MockedAuthenticatedUser? = null,
-    ): WebTestClient.ResponseSpec
+        user: MockedAuthenticatedUser? = null
+    ): WebTestClient.ResponseSpec {
+        return get("$baseUrl/me", user)
+    }
 
 
     fun logout(
-        user: MockedAuthenticatedUser? = null,
-    ): WebTestClient.ResponseSpec
+        user: MockedAuthenticatedUser? = null
+    ): WebTestClient.ResponseSpec {
+        return delete("$baseUrl/logout", user)
+    }
 }
