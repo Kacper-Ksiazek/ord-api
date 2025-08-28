4894b5f4a3fa50b63045dd2387c09f8181852e61
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-08-28 23:55:47 +0200

    feat: add MockedAuthenticatedUserUpdated data class with ResponseCookie for improved cookie handling

--- src/test/kotlin/com/ord/testing_utils/dto/MockedAuthenticatedUser.kt
+++ src/test/kotlin/com/ord/testing_utils/dto/MockedAuthenticatedUser.kt
@@ -2,10 +2,18 @@ package com.ord.testing_utils.dto
 
 import com.ord.core.user.model.UserDTO
 import jakarta.servlet.http.Cookie
+import org.springframework.http.ResponseCookie
 
 data class MockedAuthenticatedUser(
     var token: String,
     var email: String,
     var userInfo: UserDTO,
     var authCookie: Cookie
+)
+
+data class MockedAuthenticatedUserUpdated(
+    var token: String,
+    var email: String,
+    var userInfo: UserDTO,
+    var authCookie: ResponseCookie
 )
