5af61e2719d59d32c89c3b0d33f10c6e9fb774d3
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-09-05 18:47:12 +0200

    feat(BankGroup): Migrate from MVC to WebFlux

--- src/main/kotlin/com/ord/features/bank_group/repository/BankGroupRepository.kt
+++ src/main/kotlin/com/ord/features/bank_group/repository/BankGroupRepository.kt
@@ -2,12 +2,7 @@ package com.ord.features.bank_group.repository
 
 import com.ord.features.bank_group.model.BankGroupEntity
 import com.ord.shared.repositories.UserResourceRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
-interface BankGroupRepository : UserResourceRepository<BankGroupEntity> {
-    @Query("SELECT bg FROM BankGroupEntity bg WHERE bg.user.id = :userId AND lower(bg.name) LIKE lower(concat('%', :phrase, '%'))")
-    fun findAllForUserBySearchingPhrase(userId: UUID, phrase: String): List<BankGroupEntity>
-}
+interface BankGroupRepository : UserResourceRepository<BankGroupEntity>
